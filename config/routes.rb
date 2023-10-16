ee2837a9ad590bc9b623e8ecbbbbf2d45f71c7d4
othniel2471 <othnielomaidu@gmail.com>
2023-10-16 12:31:07 +0100

    fixed linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "categories#index"
   resources :expenses
   resources :categories
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "categories#index"
 end
